298801dc67a9e6f90de8157829efd7292f1a597c
Rajat Arora <rajat@rajatarora.info>
2019-10-27 19:17:19 +0530

    Add a commit to trigger build

--- src/main/java/co/rajat/demospring/DemoSpringApplication.java
+++ src/main/java/co/rajat/demospring/DemoSpringApplication.java
@@ -3,6 +3,7 @@ package co.rajat.demospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Spring Boot Application
 @SpringBootApplication
 public class DemoSpringApplication {
 
